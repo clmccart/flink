05798d13bb2ace46ff12a702cc7ad6723e45f6b1
Claire McCarthy <clairemccarthy@google.com>
2024-02-13 22:42:34 +0000

    add quickstart dockerfile and job definition

--- quickstart/src/main/java/org/myorg/quickstart/DataStreamJob.java
+++ quickstart/src/main/java/org/myorg/quickstart/DataStreamJob.java
@@ -18,7 +18,12 @@
 
 package org.myorg.quickstart;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.MultipleParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 /**
  * Skeleton for a Flink DataStream Job.
@@ -35,32 +40,94 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class DataStreamJob {
 
     public static void main(String[] args) throws Exception {
-        // Sets up the execution environment, which is the main entry point
-        // to building Flink applications.
+        System.out.println("CLAIRE TEST running empty main");
+        final MultipleParameterTool params = MultipleParameterTool.fromArgs(args);
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.getConfig().setGlobalJobParameters(params);
+        String[] words =
+                new String[] {
+                    "To be, or not to be,--that is the question:--",
+                    "Whether 'tis nobler in the mind to suffer",
+                    "The slings and arrows of outrageous fortune",
+                    "Or to take arms against a sea of troubles,",
+                    "And by opposing end them?--To die,--to sleep,--",
+                    "No more; and by a sleep to say we end",
+                    "The heartache, and the thousand natural shocks",
+                    "That flesh is heir to,--'tis a consummation",
+                    "Devoutly to be wish'd. To die,--to sleep;--",
+                    "To sleep! perchance to dream:--ay, there's the rub;",
+                    "For in that sleep of death what dreams may come,",
+                    "When we have shuffled off this mortal coil,",
+                    "Must give us pause: there's the respect",
+                    "That makes calamity of so long life;",
+                    "For who would bear the whips and scorns of time,",
+                    "The oppressor's wrong, the proud man's contumely,",
+                    "The pangs of despis'd love, the law's delay,",
+                    "The insolence of office, and the spurns",
+                    "That patient merit of the unworthy takes,",
+                    "When he himself might his quietus make",
+                    "With a bare bodkin? who would these fardels bear,",
+                    "To grunt and sweat under a weary life,",
+                    "But that the dread of something after death,--",
+                    "The undiscover'd country, from whose bourn",
+                    "No traveller returns,--puzzles the will,",
+                    "And makes us rather bear those ills we have",
+                    "Than fly to others that we know not of?",
+                    "Thus conscience does make cowards of us all;",
+                    "And thus the native hue of resolution",
+                    "Is sicklied o'er with the pale cast of thought;",
+                    "And enterprises of great pith and moment,",
+                    "With this regard, their currents turn awry,",
+                    "And lose the name of action.--Soft you now!",
+                    "The fair Ophelia!--Nymph, in thy orisons",
+                    "Be all my sins remember'd."
+                };
 
-        /*
-         * Here, you can start creating your execution plan for Flink.
-         *
-         * Start with getting some data from the environment, like
-         * 	env.fromSequence(1, 10);
-         *
-         * then, transform the resulting DataStream<Long> using operations
-         * like
-         * 	.filter()
-         * 	.flatMap()
-         * 	.window()
-         * 	.process()
-         *
-         * and many more.
-         * Have a look at the programming guide:
-         *
-         * https://nightlies.apache.org/flink/flink-docs-stable/
-         *
-         */
+        DataStream<String> text = env.fromElements(words).name("in-memory-input");
 
-        System.out.println("CLAIRE TESt");
-        // Execute program, beginning computation.
-        env.execute("Flink Java API Skeleton");
+        // The text lines read from the source are split into words
+        // using a user-defined function. The tokenizer, implemented below,
+        // will output each word as a (2-tuple) containing (word, 1)
+        DataStream<Tuple2<String, Integer>> counts =
+                text.flatMap(new Tokenizer())
+                        .name("tokenizer")
+                        // keyBy groups tuples based on the "0" field, the word.
+                        // Using a keyBy allows performing aggregations and other
+                        // stateful transformations over data on a per-key basis.
+                        // This is similar to a GROUP BY clause in a SQL query.
+                        .keyBy(value -> value.f0)
+                        // For each key, we perform a simple sum of the "1" field, the count.
+                        // If the input data stream is bounded, sum will output a final count for
+                        // each word. If it is unbounded, it will continuously output updates
+                        // each time it sees a new instance of each word in the stream.
+                        .sum(1)
+                        .name("counter");
+
+        counts.print().name("print-sink");
+
+        System.out.println("CLAIRE TEST executing");
+        env.execute();
+    }
+
+    /**
+     * Implements the string tokenizer that splits sentences into words as a user-defined
+     * FlatMapFunction. The function takes a line (String) and splits it into multiple pairs in the
+     * form of "(word,1)" ({@code Tuple2<String, Integer>}).
+     */
+    public static final class Tokenizer
+            implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<>(token, 1));
+                }
+            }
+        }
     }
 }
